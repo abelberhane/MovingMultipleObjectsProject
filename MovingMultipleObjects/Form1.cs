15e385e11421b6fcd0fa087c7f6f97ebd699a870
Abel Berhane <aberhane@datadimensions.com>
2017-09-13 10:31:23 -0400

    Two working labels

--- MovingMultipleObjects/Form1.cs
+++ MovingMultipleObjects/Form1.cs
@@ -16,6 +16,9 @@ namespace MovingMultipleObjects
         int L1x = 5;
         int L1y = 5;
 
+        int L2x = 5;
+        int L2y = 5;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +26,7 @@ namespace MovingMultipleObjects
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            // Label 1 Movement Logic
+            // Label 1 Movement Logic Begins
             label1.Left += L1x;
             label1.Top += L1y;
 
@@ -38,6 +41,26 @@ namespace MovingMultipleObjects
             {
                 L1y = -L1y;
             }
+            // Label 1 Movement Logic Complete
+
+
+
+            // Label 2 Movement Logic Begins
+            label2.Left += L2x;
+            label2.Top += L2y;
+
+            // Inverts Direction if the label hits the border Width from left to right
+            if (label2.Left + label2.Width > ClientSize.Width || label2.Left < 0)
+            {
+                L2x = -L2x;
+            }
+
+            // Inverts Direction if the labe hits the border Height from top to bottom
+            if (label2.Top + label2.Height > ClientSize.Height || label2.Top < 0)
+            {
+                L2y = -L2y;
+            }
+            // Label 2 Movement Logic Complete
         }
     }
 }
