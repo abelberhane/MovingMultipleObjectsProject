f01119a8d51f3727934a16f0ead18eb580e1fbb3
Abel Berhane <aberhane@datadimensions.com>
2017-09-13 10:27:05 -0400

    First Moving Label

--- MovingMultipleObjects/Form1.cs
+++ MovingMultipleObjects/Form1.cs
@@ -23,8 +23,21 @@ namespace MovingMultipleObjects
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            // Label 1 Movement Logic
             label1.Left += L1x;
             label1.Top += L1y;
+
+            // Inverts Direction if the label hits the border Width from left to right
+            if (label1.Left + label1.Width > ClientSize.Width || label1.Left < 0)
+            {
+                L1x = -L1x;
+            }
+
+            // Inverts Direction if the labe hits the border Height from top to bottom
+            if (label1.Top + label1.Height > ClientSize.Height || label1.Top < 0)
+            {
+                L1y = -L1y;
+            }
         }
     }
 }
